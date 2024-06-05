3b7b364bcc9e94e9addfee6fc9f3b53299b2d365
José Rodríguez <84817384+rodriguezje96@users.noreply.github.com>
2024-06-05 14:02:35 -0300

    No esta reaccionando al mandar el start

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ dotenv.config();
 const PORT = process.env.PORT || 8000;
 const botToken = process.env.BOT_TOKEN;
 const webToScrap = "https://www.argenprop.com/departamentos-o-ph/alquiler/capital-federal?con-ambiente-balcon&con-permitemascotas&hasta-600000-pesos";
-const botActive = process.env.BOT_ACTIVE === 'true';
 
 if (!botToken) {
     console.error("Telegram Bot Token not provided!");
@@ -20,54 +19,50 @@ if (!botToken) {
 const bot = new TelegramBot(botToken, { polling: true });
 const app = express();
 
-const mensajePrueba = 'Hola! Soy el Bot de Alquileres y estoy probando si esto funca';
 const activeChatIds = {};
-
 let maxPrice = null; // Variable para almacenar el precio máximo
 let location = null; // Variable para almacenar la ubicación
 
-if (botActive) {
-    bot.onText(/\/start/, (msg) => {
-        const chatId = msg.chat.id;
-        activeChatIds[chatId] = true;
-        const welcomeMessage = `¡Bienvenido al Bot de Alquileres! \n\n
-        Este bot te ayuda a buscar alquiler de departamentos en Capital Federal. \n
-        Para empezar, podés utilizar los siguientes comandos:
-        - /setmaxprice [precio]: Decime el máximo de guita que podés gastar, tirate a más por las dudas.
-        - /setlocation [barrio]: Si sos platudo y querés mirar por ubicación decime eso directamente. \n\n
-        ¡Adelante, elegí. Estoy seguro que perderás!`;
-        bot.sendMessage(chatId, welcomeMessage);
-        console.log('Usuario saludado con /start');
-    });
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    activeChatIds[chatId] = true;
+    const welcomeMessage = `¡Bienvenido al Bot de Alquileres! \n\n
+    Este bot te ayuda a buscar alquiler de departamentos en Capital Federal. \n
+    Para empezar, podés utilizar los siguientes comandos:
+    - /setmaxprice [precio]: Decime el máximo de guita que podés gastar, tirate a más por las dudas.
+    - /setlocation [barrio]: Si sos platudo y querés mirar por ubicación decime eso directamente. \n\n
+    ¡Adelante, elegí. Estoy seguro que perderás!`;
+    bot.sendMessage(chatId, welcomeMessage);
+    console.log('Usuario saludado con /start');
+});
 
-    bot.on('message', (msg) => {
-        const chatId = msg.chat.id;
-        const command = msg.text.toLowerCase();
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const command = msg.text.toLowerCase();
 
-        if (command.startsWith('/setmaxprice')) {
-            const newMaxPriceString = command.split(' ')[1];
-            const newMaxPrice = parseInt(newMaxPriceString.replace(/\./g, ''));
-            if (!isNaN(newMaxPrice)) {
-                maxPrice = newMaxPrice;
-                bot.sendMessage(chatId, `Podés gastar: $${maxPrice}`);
-                scrapeData();
-            } else {
-                bot.sendMessage(chatId, 'Revisá lo que mandaste, solo entiendo números.');
-            }
-        } else if (command.startsWith('/setlocation')) {
-            const newLocation = command.split(' ')[1];
-            if (isNaN(newLocation)) {
-                location = newLocation.toLowerCase(); // Guardar la ubicación en minúsculas
-                bot.sendMessage(chatId, `Te querés mudar a: ${location}`);
-                scrapeData();
-            } else {
-                bot.sendMessage(chatId, 'Ese barrio no lo conozco, probá con otro.');
-            }
-        } else if (!command.startsWith('/start')) {
-            bot.sendMessage(chatId, 'No reconozco ese comando. Por favor, utilizá /start, /setmaxprice o /setlocation.');
+    if (command.startsWith('/setmaxprice')) {
+        const newMaxPriceString = command.split(' ')[1];
+        const newMaxPrice = parseInt(newMaxPriceString.replace(/\./g, ''));
+        if (!isNaN(newMaxPrice)) {
+            maxPrice = newMaxPrice;
+            bot.sendMessage(chatId, `Podes gastar: $${maxPrice}`);
+            scrapeData();
+        } else {
+            bot.sendMessage(chatId, 'Revisá lo que mandaste, solo entiendo números.');
         }
-    });
-}
+    } else if (command.startsWith('/setlocation')) {
+        const newLocation = command.split(' ')[1];
+        if (isNaN(newLocation)) {
+            location = newLocation.toLowerCase(); // Guardar la ubicación en minúsculas
+            bot.sendMessage(chatId, `Te queres mudar a: ${location}`);
+            scrapeData();
+        } else {
+            bot.sendMessage(chatId, 'Ese barrio no lo conozco, probá con otro.');
+        }
+    } else if (!command.startsWith('/start')) {
+        bot.sendMessage(chatId, 'No reconozco ese comando. Por favor, utilizá /start, /setmaxprice o /setlocation.');
+    }
+});
 
 function enviarMensaje(chatId, mensaje) {
     bot.sendMessage(chatId, mensaje)
